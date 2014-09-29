22c9271b41bfb10f07c92b1e768b8528f2fdb146
sagar <sagar.chandarana123@gmail.com>
2014-09-30 02:17:46 +0530

    app.js: 1) removed deprecated express calls, 2) oauth initial configs 3) endpoint to receive credentials

--- app.js
+++ app.js
@@ -10,25 +10,23 @@ var oauth = require('oauthio');
 var app = express();
 
 app.use(express.static('public'));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(session({secret: 'keyboard cat', key: 'sid'}));
+app.use(session({
+    secret: 'keyboard cat',
+    saveUninitialized: true, 
+    resave: true
+}	));
 
 /* Initialization */
-try {
-	var config = require('./config');	
-} catch (e) {
-	// Create a config.js file returning an object like the following if you haven't done it yet
-	var config = {
-		key: 'your_key',
-		secret: 'your_secret'
-	};
-}
-
-oauth.initialize(config.key, config.secret);
+var oauth = require('oauthio');
+oauth.setOAuthdUrl("http://localhost:6284", '/');
+oauth.initialize('70498cfce63946eea646e98e3d6a7644', '70498cfce63946eea646e98e3d6a7644');
 
 /* Endpoints */
-
 app.get('/oauth/token', function (req, res) {
 	var token = oauth.generateStateToken(req.session);
 
@@ -37,9 +35,9 @@ app.get('/oauth/token', function (req, res) {
 	});
 });
 
-
 app.post('/oauth/signin', function (req, res) {
 	var code = req.body.code;
+	console.log('signing in:', req.session);
 	oauth.auth('google', req.session, {
 		code: code
 	})
@@ -48,14 +46,20 @@ app.post('/oauth/signin', function (req, res) {
 		// for the requested provider is stored in the session.
 		// Continue the tutorial or checkout the step-4 to get
 		// the code for the request
-		res.send(200, 'The user is authenticated');
+		
+		//TODO: give only a few things
+		res.status(200).send(request_object.getCredentials());
 	})
 	.fail(function (e) {
-		console.log(e);
-		res.send(400, 'Code is incorrect');
+		console.log('signin', e);
+		res.status(400).send(e);
 	});
 });
 
+app.get('/random', function(req, res) {
+	console.log('random:', req.session);
+	res.status(200).send('haha');
+});
 
 app.get('/me', function (req, res) {
 	// Here we first build a request object from the session with the auth method.
@@ -63,16 +67,17 @@ app.get('/me', function (req, res) {
 	// This retrieves a unified object representing the authenticated user.
 	// You could also use .get('/plus/v1/people/me') and map the results to fields usable from
 	// the front-end (which waits for the fields 'name', 'email' and 'avatar').
+	console.log('fetching me:', req.session);
 	oauth.auth('google', req.session)
 	.then(function (request_object) {
-		return request_object.me();
+		return request_object.get('/plus/v1/people/me');
 	})
 	.then(function (user_data) {
 		res.json(user_data);
 	})
 	.fail(function (e) {
-		console.log(e);
-		res.send(400, 'An error occured');
+		console.log('me', e);
+		res.status(400).send(e);
 	});
 });
 

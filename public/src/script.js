cc21d850dc2d6f3d03f5cfd47cc9878a5a398476
sagar <sagar.chandarana123@gmail.com>
2014-10-02 03:18:49 +0530

    1) server:  signin endpoint hardcoded for google: client receives: provider token, 'me', and appbase token, 2) client script: generates arequest object

--- public/src/script.js
+++ public/src/script.js
@@ -29,7 +29,9 @@ function authenticate(token, callback) {
 				url: '/oauth/signin',
 				method: 'POST',
 				data: {
-					code: r.code
+				    app: credentials.key,
+					code: r.code,
+					secret: credentials.secret
 				},
 				success: function(data, status) {
 					callback(null, data);
@@ -44,12 +46,10 @@ function authenticate(token, callback) {
 		});
 }
 
-function retrieve_user_info(creds, callback) {
-	console.log('creds', creds);
-	var request = OAuth.create('google', creds);
-	request.get('/plus/v1/people/me')
-	.done( callback)
-	.fail( console.error.bind(console));
+function retrieve_user_info(data, callback) {
+	console.log('creds', data);
+	var request = reqObj = OAuth.create('google', data.credentials.provider);
+	callback(data)
 }
 
 function random() {
@@ -63,9 +63,9 @@ function go() {
 		authenticate(token, function(err, creds) {
 			if (!err) {
 				retrieve_user_info(creds, function(user_data) {
-					$('#name_box').html(user_data.displayName)
-					$('#email_box').html(user_data.emails[0].value);
-					$('#img_box').attr('src', user_data.image.url);
+					$('#name_box').html(user_data.firstname)
+					$('#email_box').html(user_data.email);
+					$('#img_box').attr('src', user_data.avatar);
 				});
 			} else {
 				console.error(err)
